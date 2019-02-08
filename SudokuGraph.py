1ef43154924378be7b43bb2e74b0cf9ff5d79731
Rodrigo Arriaza <arr15334@uvg.edu.gt>
2019-02-07 20:02:01 -0600

    add check colum

--- SudokuGraph.py
+++ SudokuGraph.py
@@ -1,4 +1,5 @@
 class SudokuGraph(object):
+    all_sdk = [1,2,3,4]
     def __init__(self):
         self.initial_state = []
 
@@ -20,7 +21,6 @@ class SudokuGraph(object):
 
     def goalTest(self, s):
         sdku_num = [[0 for x in range(0,4)] for y in range(0,4)]
-        all_sdk = [1,2,3,4]
         for i in range(0,4):
             for j in range(0,4):
                 if s[i][j] == ".":
@@ -35,9 +35,8 @@ class SudokuGraph(object):
                 print(sdku_num[i])
                 print("suma")
                 return False
-        # revisar todas las columnas
-        #for i in range(0,4):
-            #return
+        if (not checkColumns(self, s)):
+            return False
         # revisar 4 mini grids  
         return True
 
@@ -46,3 +45,32 @@ class SudokuGraph(object):
 
     def pathCost(self, states):
         return 0
+
+    def checkColumns(self, state):
+        col_amount = len(state[0])
+        for i in range(0, col_amount):
+            curr_col = []
+            for j in range(0, col_amount):
+                curr_col.append(state[i][j])
+            if not all(x in curr_col for x in all_sdk):
+                return False
+        return True
+
+    def checkGrid(self, state):
+        size = 1 if len(state[0] == 4) else 3
+        start = 0
+        for i in range(start, size):
+            curr_grid = []
+            for j in range(start, size):
+                curr_grid.append(state[i][j])
+            if not all(x in curr_grid for x in all_sdk):
+                return False
+            size += 1
+            # 00 01 10 11 / 02 03 12 13 / 20 21 30 31 / 22 23 32 33
+            # 00 01 02 10 11 12 20 21 22 / 
+        return size
+
+
+# [1,2,3,4],[1,2,3,4],[1,2,3,4],[1,2,3,4]
+# [1,2,3,4,5,6,7,8,9],[1,2,3,4,5,6,7,8,9],[1,2,3,4,5,6,7,8,9],[1,2,3,4,5,6,7,8,9],[1,2,3,4,5,6,7,8,9],[1,2,3,4,5,6,7,8,9],[1,2,3,4,5,6,7,8,9],[1,2,3,4,5,6,7,8,9],[1,2,3,4,5,6,7,8,9]
+# 
